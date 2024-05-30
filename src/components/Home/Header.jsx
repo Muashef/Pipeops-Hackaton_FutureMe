cad4ab1934259345af0f08d705f001fd4fd1df0f
Crusader <abdullah.mudathir@isslng.com>
2024-05-30 12:09:21 +0100

    working: working on the header section

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import background from "../../assets/hone_bg.png";
+import bgBottomLeft from "../../assets/header__bg.png";
+import bgBottomRight from "../../assets/header__bg2.png";
 import test from "../../assets/blob.svg";
 
 export function Header() {
   return (
     <div className="header">
       <img src={background} className="header__bg" alt="" />
+      <img src={bgBottomLeft} className="bg__header bg__left" alt="" />
+      <img src={bgBottomRight} className="bg__header bg__right" alt="" />
       <div>
         <p>Discover Your Future</p>
         <h1>Career Path Unlock</h1>

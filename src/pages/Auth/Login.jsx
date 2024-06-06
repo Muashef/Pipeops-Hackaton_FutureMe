751120e947307523c46f3031842c3e3d1b58b1f8
Sherif <muhammedsherif40@yahoo.com>
2024-06-06 10:48:07 +0100

    feat:auth functionality

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -4,9 +4,17 @@ import Goggle from "../../assets/google.svg";
 import FB from "../../assets/fb.svg";
 import Apple from "../../assets/apple.svg";
 import { CiMail } from "react-icons/ci";
+import { ToastContainer, Zoom, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.config";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -18,21 +26,59 @@ const Login = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleLogin = async (e) => {
+
+  const handleLogin = (e) => {
     e.preventDefault();
-    setLoading(true);
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        //TO DO : Handle case after sign in
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // TODO: Handle error case.
-      })
-      .finally(() => {
-        setLoading(false);
+    if (validate()) {
+      setLoading(true);
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log(userCredential);
+
+          navigate("/dashboard");
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+          if (
+            error.code === "auth/wrong-password" ||
+            error.code === "auth/invalid-email"
+          ) {
+            toast.error("Incorrect email or password. Please try again.", {
+              theme: "colored",
+            });
+          } else if (error.code === "auth/invalid-credential") {
+            toast.error("Incorrect Email Address or Password.", {
+              theme: "colored",
+            });
+          } else {
+            toast.error("An error occurred. Please try again later.", {
+              theme: "colored",
+            });
+            console.error(error);
+          }
+        });
+    }
+  };
+
+  const validate = () => {
+    let result = true;
+    if (email === "" || email === null) {
+      result = false;
+      toast.warning("Please Enter Email Address", {
+        theme: "light",
+        autoClose: 3000,
       });
+    }
+    if (password === "" || password === null) {
+      result = false;
+      toast.error("Please Enter Password", {
+        theme: "light",
+        autoClose: 3000,
+      });
+    }
+    return result;
   };
 
   return (
@@ -41,7 +87,7 @@ const Login = () => {
         <img className="object-cover h-full w-full" src={AuthLogo} alt="Logo" />
       </div>
       <div className="w-full md:w-1/2 flex md:px-16 bg-white overflow-y-auto justify-center flex-col max-md:items-center">
-        <div className="max-w-md p-6 mt-32 md:mt-28">
+        <div className="max-w-md p-6 mt-32 md:mt-20">
           <h2 className="text-2xl text-center text-[#334158] font-semibold mb-2">
             Sign into your account
           </h2>
@@ -83,16 +129,16 @@ const Login = () => {
                   Forgot password?
                 </Link>
               </div>
-              <div className="absolute inset-y-0 right-0 pr-3 top-5 flex items-center">
+              <div className="absolute inset-y-0 left-3 top-6 pr-3 flex items-center">
                 {showPassword ? (
                   <IoEyeOutline
-                    className="h-4 w-4 text-gray-400 cursor-pointer"
+                    className="h-5 w-5 text-gray-400 cursor-pointer"
                     onClick={togglePasswordVisibility}
                     aria-hidden="true"
                   />
                 ) : (
                   <IoEyeOffOutline
-                    className="h-4 w-4 text-gray-400 cursor-pointer"
+                    className="h-5 w-5 text-gray-400 cursor-pointer"
                     onClick={togglePasswordVisibility}
                     aria-hidden="true"
                   />
@@ -101,7 +147,7 @@ const Login = () => {
               <input
                 id="password-input"
                 type={showPassword ? "text" : "password"}
-                className="rounded-md px-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
+                className="rounded-md pl-10 pr-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
                 name="password"
                 required
                 autoComplete="off"
@@ -112,6 +158,7 @@ const Login = () => {
             </div>
             <button
               type="submit"
+              onClick={handleLogin}
               className="w-full py-3 px-4 mt-8 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#F97699]"
             >
               {loading ? "Loading" : "Sign In"}
@@ -127,7 +174,7 @@ const Login = () => {
           <div className="flex items-center justify-center space-x-4 mt-4 mb-4 cursor-pointer">
             <img src={Goggle} alt="google" />
             <img src={FB} alt="facebook" />
-            <img src={Apple} alt="apple" />
+            {/* <img src={Apple} alt="apple" /> */}
           </div>
           <p className="text-[#334158] text-base font-normal mt-8 text-center">
             Are you new here?
@@ -139,6 +186,7 @@ const Login = () => {
             </Link>
           </p>
         </div>
+        <ToastContainer transition={Zoom} />
       </div>
     </div>
   );
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -4,9 +4,14 @@ import Goggle from "../../assets/google.svg";
 import FB from "../../assets/fb.svg";
 import Apple from "../../assets/apple.svg";
 import { CiMail } from "react-icons/ci";
+import { LuUser2 } from "react-icons/lu";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, Zoom, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import auth from "../../../firebase.config";
+
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
@@ -19,23 +24,72 @@ const Signup = () => {
     setShowPassword(!showPassword);
   };
 
+
   const handleSignUp = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        const user = userCredential.user;
-        updateProfile(user, {
-          displayName: name,
-        });
-        // TODO : Handle case after signup
-      })
-      .catch((error) => {
-        // Handle error
-      })
-      .finally(() => {
+    if (validate()) {
+      setLoading(true);
+      try {
+        const response = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        console.log(response);
+        setTimeout(() => {
+          navigate("/login");
+        }, 1500);
         setLoading(false);
+        toast.success("Account created successfully", {
+          theme: "colored",
+          autoClose: 1500,
+        });
+      } catch (error) {
+        setLoading(false);
+        console.error(error.message);
+        if (error.code === "auth/email-already-in-use") {
+          toast.error("User with this email already exists. Please log in.", {
+            theme: "colored",
+            autoClose: 1500,
+          });
+          setTimeout(() => {
+            navigate("/login");
+          }, 1500);
+        } else if (error.code === "auth/weak-password") {
+          toast.error("Password should be at least 8 characters.", {
+            theme: "colored",
+          });
+        }
+      }
+    }
+  };
+
+  const validate = () => {
+    let result = true;
+    if (username === "" || username === null) {
+      result = false;
+      toast.warning("Please Enter a Username", {
+        theme: "light",
+        autoClose: 3000,
       });
+    }
+    if (email === "" || email === null) {
+      result = false;
+      toast.warning("Please Enter Email Address", {
+        theme: "light",
+        autoClose: 3000,
+      });
+    }
+    if (password === "" || password === null) {
+      result = false;
+      toast.warning("Please Enter Password", {
+        theme: "light",
+        autoClose: 3000,
+      });
+    }
+
+    return result;
   };
 
   return (
@@ -49,17 +103,20 @@ const Signup = () => {
             Create Your Account
           </h2>
           <form className="mt-5">
-            <div className="mb-4">
+            <div className="mb-4 relative">
               <label
                 htmlFor="name"
                 className="text-lg font-normal text-[#334158]"
               >
                 Name
               </label>
-              <input
+              <div className="absolute inset-y-0 left-3 top-6 pr-3 flex items-center pointer-events-none">
+                <LuUser2 className="h-6 w-6 text-gray-400" aria-hidden="true" />
+              </div>
+              <input 
                 id="username"
                 type="text"
-                className="rounded-md px-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
+                className="rounded-md pl-10 pr-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
                 name="username"
                 autoComplete="off"
                 autoFocus
@@ -76,13 +133,13 @@ const Signup = () => {
               >
                 Email
               </label>
-              <div className="absolute inset-y-0 right-0 top-5 pr-3 flex items-center pointer-events-none">
-                <CiMail className="h-4 w-4 text-gray-400" aria-hidden="true" />
+              <div className="absolute inset-y-0 left-3 top-6 pr-3 flex items-center pointer-events-none">
+                <CiMail className="h-6 w-6 text-gray-400" aria-hidden="true" />
               </div>
               <input
                 id="email"
                 type="text"
-                className="rounded-md px-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
+                className="rounded-md pl-10 pr-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
                 name="email"
                 autoComplete="off"
                 value={email}
@@ -98,16 +155,16 @@ const Signup = () => {
               >
                 Password
               </label>
-              <div className="absolute inset-y-0 right-0 pr-3 top-5 flex items-center">
+              <div className="absolute inset-y-0 left-3 top-6 pr-3 flex items-center">
                 {showPassword ? (
                   <IoEyeOutline
-                    className="h-4 w-4 text-gray-400 cursor-pointer"
+                    className="h-5 w-5 text-gray-400 cursor-pointer"
                     onClick={togglePasswordVisibility}
                     aria-hidden="true"
                   />
                 ) : (
                   <IoEyeOffOutline
-                    className="h-4 w-4 text-gray-400 cursor-pointer"
+                    className="h-5 w-5 text-gray-400 cursor-pointer"
                     onClick={togglePasswordVisibility}
                     aria-hidden="true"
                   />
@@ -116,7 +173,7 @@ const Signup = () => {
               <input
                 id="password-input"
                 type={showPassword ? "text" : "password"}
-                className="rounded-md px-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
+                className="rounded-md pl-9 pr-4 py-3 w-full bg-[#F8F8F8] placeholder:text-[#D6D6D6] border border-[#F8F8F8] outline-none"
                 name="password"
                 required
                 autoComplete="off"
@@ -147,6 +204,7 @@ const Signup = () => {
             </div>
             <button
               type="submit"
+              onClick={handleSignUp}
               className="w-full py-3 px-4 mt-8 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#F97699]"
             >
               {loading ? "Loading" : "Sign Up"}
@@ -162,7 +220,7 @@ const Signup = () => {
           <div className="flex items-center justify-center space-x-4 mt-4 mb-4 cursor-pointer">
             <img src={Goggle} alt="google" />
             <img src={FB} alt="facebook" />
-            <img src={Apple} alt="apple" />
+            {/* <img src={Apple} alt="apple" /> */}
           </div>
           <p className="text-[#334158] text-base font-normal mt-8 text-center">
             Already have an account?
@@ -174,6 +232,7 @@ const Signup = () => {
             </Link>
           </p>
         </div>
+        <ToastContainer transition={Zoom} />
       </div>
     </div>
   );
